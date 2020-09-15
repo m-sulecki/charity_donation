5f87aec2bdea552d08d42886337eadbfe3c827e5
MS <msulek59@hotmail.com>
2020-09-15 23:42:58 +0200

    used DRY in templates

--- charity_donation/settings.py
+++ charity_donation/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'charity_donation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -128,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- karitato/views.py
+++ karitato/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class LandingPage(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+
+class AddDonationConfiramtion(View):
+    def get(self, request):
+        return render(request, 'form-confirmation.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
