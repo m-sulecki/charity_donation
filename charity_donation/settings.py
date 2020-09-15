455c2d567453f03c01b7ae81318053bd52e4f4a9
MS <msulek59@hotmail.com>
2020-09-15 18:07:05 +0200

    added app in settings

--- charity_donation/settings.py
+++ charity_donation/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'karitato.apps.KaritatoConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
