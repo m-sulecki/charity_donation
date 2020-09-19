fd6b3eba182958c5e7346059ddfbcc0a5c36c916
MS <msulek59@hotmail.com>
2020-09-19 12:39:43 +0200

    Revert "created Institution model"
    This reverts commit dd1173eb4433e990b5f992b8c7bdbd33a0fd2be7.

--- karitato/models.py
+++ karitato/models.py
@@ -2,16 +2,3 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, default="")
-
-TYPES = (
-    (0, "fundacja"),
-    (1, "organizacja pozarządowa"),
-    (2, "zbiórka lokalna"),
-)
-
-class Institution(models.Model):
-    name = models.CharField(max_length=128, default="")
-    description = models.TextField(default="")
-    type = models.IntegerField(choices=TYPES, default=0)
-    categories = models.ManyToManyField(Category, )
-
