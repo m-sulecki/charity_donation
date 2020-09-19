ba29576af2b37a678a6c1e6f6979089545413728
MS <msulek59@hotmail.com>
2020-09-19 18:00:22 +0200

    added anchor links

--- karitato/urls.py
+++ karitato/urls.py
@@ -3,8 +3,8 @@ from .views import LandingPage, LoginView, RegisterView, AddDonation, AddDonatio
 
 urlpatterns = [
     path('', LandingPage.as_view(), name='index'),
-    path('login', LoginView.as_view(), name='login'),
-    path('register', RegisterView.as_view(), name='register'),
-    path('form', AddDonation.as_view(), name='form'),
-    path('form/confirmation', AddDonationConfiramtion.as_view(), name='form-pass')
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('form/', AddDonation.as_view(), name='form'),
+    path('form/confirmation/', AddDonationConfiramtion.as_view(), name='form-pass')
 ]
